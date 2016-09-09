cb25593bbbb198c8aee5820ae0bc8e820745b0df
James Corkran <jcorkran@partnerfusion.com>
2016-09-09 12:53:01 -0600

    Adding in package testing for java

--- Eps01-Scripts_to_Recipes/tomcat/test/integration/default/serverspec/default_spec.rb
+++ Eps01-Scripts_to_Recipes/tomcat/test/integration/default/serverspec/default_spec.rb
@@ -1,7 +1,11 @@
 require 'spec_helper'
 
 describe 'tomcat::default' do
-	describe command("curl http://localhost:8080") do
+	describe command('curl http://localhost:8080') do
 		its(:stdout) { should match /Tomcat/ }
+	end
+
+	describe package('java-1.7.0-openjdk-devel') do
+		it { should be_installed }
 	end
 end
